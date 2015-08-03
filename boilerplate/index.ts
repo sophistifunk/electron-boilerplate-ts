c170603c818cf08e5b9d9f4b291989e8f5977344
Josh McDonald <josh@joshmcdonald.info>
2015-08-03 21:59:52 +1000

    Working

--- boilerplate/index.ts
+++ boilerplate/index.ts
@@ -1,10 +1,8 @@
 /// <reference path="typings/node/node.d.ts" />
 /// <reference path="typings/github-electron/github-electron-main.d.ts" />
 
-//import app = require('app');
+import app = require('app');
 import BrowserWindow = require('browser-window');
-
-
 
 // report crashes to the Electron project
 require('crash-reporter').start();
@@ -14,37 +12,37 @@ require('electron-debug')();
 
 var mainWindow:GitHubElectron.BrowserWindow;
 
-//function createMainWindow () {
-//	const win = new BrowserWindow({
-//		width: 600,
-//		height: 400,
-//		resizable: false
-//	});
-//
-//	win.loadUrl(`file://${__dirname}/index.html`);
-//	win.on('closed', onClosed);
-//
-//	return win;
-//}
-//
-//function onClosed() {
-//	// deref the window
-//	// for multiple windows store them in an array
-//	mainWindow = null;
-//}
+function createMainWindow () {
+	const win = new BrowserWindow({
+		width: 600,
+		height: 400,
+		resizable: false
+	});
 
-//app.on('window-all-closed', function () {
-//	if (process.platform !== 'darwin') {
-//		app.quit();
-//	}
-//});
-//
-//app.on('activate-with-no-open-windows', function () {
-//	if (!mainWindow) {
-//		mainWindow = createMainWindow();
-//	}
-//});
-//
-//app.on('ready', function () {
-//	mainWindow = createMainWindow();
-//});
+	win.loadUrl(`file://${__dirname}/index.html`);
+	win.on('closed', onClosed);
+
+	return win;
+}
+
+function onClosed() {
+	// deref the window
+	// for multiple windows store them in an array
+	mainWindow = null;
+}
+
+app.on('window-all-closed', function () {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate-with-no-open-windows', function () {
+	if (!mainWindow) {
+		mainWindow = createMainWindow();
+	}
+});
+
+app.on('ready', function () {
+	mainWindow = createMainWindow();
+});
